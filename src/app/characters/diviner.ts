a1238d730943110c052314d90c86ade9a19b9013
Logan Nitta <logan.nitta@gmail.com>
2022-07-27 17:57:53 -0600

    Diviner removeActions

--- src/app/characters/diviner.ts
+++ src/app/characters/diviner.ts
@@ -61,9 +61,21 @@ export const divinerActions = {
     removeCard(CardId.minus1, newDeck, [divinerCards.plus1Heal2Ally]);
     return newDeck;
   },
+  'Undo Replace one (-1) card with one (+1) [Heal 2 Ally] card': (
+    deck: Card[]
+  ) => {
+    const newDeck = [...deck];
+    removeCard(CardId.divinerPlus1Heal2Ally, newDeck, [basicCards.minus1]);
+    return newDeck;
+  },
   'Replace one (+0) card with one (+2) [Dark] card': (deck: Card[]) => {
     const newDeck = [...deck];
     removeCard(CardId.plus0, newDeck, [divinerCards.plus2Dark]);
+    return newDeck;
+  },
+  'Undo Replace one (+0) card with one (+2) [Dark] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    removeCard(CardId.divinerPlus2Dark, newDeck, [basicCards.plus0]);
     return newDeck;
   },
   'Replace one (+0) card with one (+2) [Light] card': (deck: Card[]) => {
@@ -71,14 +83,29 @@ export const divinerActions = {
     removeCard(CardId.plus0, newDeck, [divinerCards.plus2Light]);
     return newDeck;
   },
+  'Undo Replace one (+0) card with one (+2) [Light] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    removeCard(CardId.divinerPlus2Light, newDeck, [basicCards.plus0]);
+    return newDeck;
+  },
   'Replace one (+0) card with one (+2) [Curse] card': (deck: Card[]) => {
     const newDeck = [...deck];
     removeCard(CardId.plus0, newDeck, [divinerCards.plus2Curse]);
     return newDeck;
   },
+  'Undo Replace one (+0) card with one (+2) [Curse] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    removeCard(CardId.divinerPlus2Curse, newDeck, [basicCards.plus0]);
+    return newDeck;
+  },
   'Replace one (+0) card with one (+3) [Muddle] card': (deck: Card[]) => {
     const newDeck = [...deck];
     removeCard(CardId.plus0, newDeck, [divinerCards.plus3Muddle]);
+    return newDeck;
+  },
+  'Undo Replace one (+0) card with one (+3) [Muddle] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    removeCard(CardId.divinerPlus3Muddle, newDeck, [basicCards.plus0]);
     return newDeck;
   },
   'Replace one (+0) card with one (+2) [Regenerate Self] card': (
@@ -88,11 +115,25 @@ export const divinerActions = {
     removeCard(CardId.plus0, newDeck, [divinerCards.plus2RegenerateSelf]);
     return newDeck;
   },
+  'Undo Replace one (+0) card with one (+2) [Regenerate Self] card': (
+    deck: Card[]
+  ) => {
+    const newDeck = [...deck];
+    removeCard(CardId.divinerPlus2RegenerateSelf, newDeck, [basicCards.plus0]);
+    return newDeck;
+  },
   'Replace one (+0) card with one (+1) [Shield 1 Ally] card': (
     deck: Card[]
   ) => {
     const newDeck = [...deck];
     removeCard(CardId.plus0, newDeck, [divinerCards.plus1Shield1Ally]);
+    return newDeck;
+  },
+  'Undo Replace one (+0) card with one (+1) [Shield 1 Ally] card': (
+    deck: Card[]
+  ) => {
+    const newDeck = [...deck];
+    removeCard(CardId.divinerPlus1Shield1Ally, newDeck, [basicCards.plus0]);
     return newDeck;
   },
   'Replace two (+1) cards with one (+3) [Shield 1 Self] card': (
@@ -103,10 +144,23 @@ export const divinerActions = {
     removeCard(CardId.plus1, newDeck, [divinerCards.plus3Shield1Self]);
     return newDeck;
   },
+  'Undo Replace two (+1) cards with one (+3) [Shield 1 Self] card': (
+    deck: Card[]
+  ) => {
+    const newDeck = [...deck];
+    removeCard(CardId.divinerPlus3Shield1Self, newDeck, [
+      basicCards.plus1,
+      basicCards.plus1,
+    ]);
+    return newDeck;
+  },
   'Remove one -2 card': (deck: Card[]) => {
     const newDeck = [...deck];
     removeCard(CardId.minus2, newDeck);
     return newDeck;
+  },
+  'Undo Remove one -2 card': (deck: Card[]) => {
+    return [...deck, basicCards.minus2, basicCards.minus2];
   },
   'Add two rolling [Heal 1 Self] cards': (deck: Card[]) => {
     return [
@@ -115,11 +169,31 @@ export const divinerActions = {
       divinerCards.rollingHeal1Self,
     ];
   },
+  'Undo Add two rolling [Heal 1 Self] cards': (deck: Card[]) => {
+    const newDeck = [...deck];
+    removeCard(CardId.divinerRollingHeal1Self, newDeck);
+    removeCard(CardId.divinerRollingHeal1Self, newDeck);
+    return newDeck;
+  },
   'Add two rolling [Curse] cards': (deck: Card[]) => {
     return [...deck, divinerCards.rollingCurse, divinerCards.rollingCurse];
   },
+  'Undo Add two rolling [Curse] cards': (deck: Card[]) => {
+    const newDeck = [...deck];
+    removeCard(CardId.divinerRollingCurse, newDeck);
+    removeCard(CardId.divinerRollingCurse, newDeck);
+    return newDeck;
+  },
   'Ignore negative scenario effects and add two (+1) cards': (deck: Card[]) => {
     return [...deck, basicCards.plus1, basicCards.plus1];
+  },
+  'Undo Ignore negative scenario effects and add two (+1) cards': (
+    deck: Card[]
+  ) => {
+    const newDeck = [...deck];
+    removeCard(CardId.plus1, newDeck);
+    removeCard(CardId.plus1, newDeck);
+    return newDeck;
   },
 };
 
@@ -132,18 +206,21 @@ export class Diviner implements Character {
       completed: false,
       text: 'Remove two (-1) cards',
       addAction: BasicPerkAction['Remove two -1 cards'],
+      removeAction: BasicPerkAction['Undo Remove two -1 cards'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Remove two (-1) cards',
       addAction: BasicPerkAction['Remove two -1 cards'],
+      removeAction: BasicPerkAction['Undo Remove two -1 cards'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Remove one (-2) card',
       addAction: divinerActions['Remove one -2 card'],
+      removeAction: divinerActions['Undo Remove one -2 card'],
     },
     {
       name: 'Primary',
@@ -153,6 +230,10 @@ export class Diviner implements Character {
         divinerActions[
           'Replace two (+1) cards with one (+3) [Shield 1 Self] card'
         ],
+      removeAction:
+        divinerActions[
+          'Undo Replace two (+1) cards with one (+3) [Shield 1 Self] card'
+        ],
     },
     {
       name: 'Primary',
@@ -161,6 +242,10 @@ export class Diviner implements Character {
       addAction:
         divinerActions[
           'Replace two (+1) cards with one (+3) [Shield 1 Self] card'
+        ],
+      removeAction:
+        divinerActions[
+          'Undo Replace two (+1) cards with one (+3) [Shield 1 Self] card'
         ],
     },
     {
@@ -171,6 +256,10 @@ export class Diviner implements Character {
         divinerActions[
           'Replace one (+0) card with one (+1) [Shield 1 Ally] card'
         ],
+      removeAction:
+        divinerActions[
+          'Undo Replace one (+0) card with one (+1) [Shield 1 Ally] card'
+        ],
     },
     {
       name: 'Primary',
@@ -178,6 +267,8 @@ export class Diviner implements Character {
       text: 'Replace one (+0) card with one (+2) [Dark] card',
       addAction:
         divinerActions['Replace one (+0) card with one (+2) [Dark] card'],
+      removeAction:
+        divinerActions['Undo Replace one (+0) card with one (+2) [Dark] card'],
     },
     {
       name: 'Primary',
@@ -185,6 +276,8 @@ export class Diviner implements Character {
       text: 'Replace one (+0) card with one (+2) [Light] card',
       addAction:
         divinerActions['Replace one (+0) card with one (+2) [Light] card'],
+      removeAction:
+        divinerActions['Undo Replace one (+0) card with one (+2) [Light] card'],
     },
     {
       name: 'Primary',
@@ -192,6 +285,10 @@ export class Diviner implements Character {
       text: 'Replace one (+0) card with one (+3) [Muddle] card',
       addAction:
         divinerActions['Replace one (+0) card with one (+3) [Muddle] card'],
+      removeAction:
+        divinerActions[
+          'Undo Replace one (+0) card with one (+3) [Muddle] card'
+        ],
     },
     {
       name: 'Primary',
@@ -199,6 +296,8 @@ export class Diviner implements Character {
       text: 'Replace one (+0) card with one (+2) [Curse] card',
       addAction:
         divinerActions['Replace one (+0) card with one (+2) [Curse] card'],
+      removeAction:
+        divinerActions['Undo Replace one (+0) card with one (+2) [Curse] card'],
     },
     {
       name: 'Primary',
@@ -207,6 +306,10 @@ export class Diviner implements Character {
       addAction:
         divinerActions[
           'Replace one (+0) card with one (+2) [Regenerate Self] card'
+        ],
+      removeAction:
+        divinerActions[
+          'Undo Replace one (+0) card with one (+2) [Regenerate Self] card'
         ],
     },
     {
@@ -217,18 +320,24 @@ export class Diviner implements Character {
         divinerActions[
           'Replace one (-1) card with one (+1) [Heal 2 Ally] card'
         ],
+      removeAction:
+        divinerActions[
+          'Undo Replace one (-1) card with one (+1) [Heal 2 Ally] card'
+        ],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add two rolling [Heal 1 Self] cards',
       addAction: divinerActions['Add two rolling [Heal 1 Self] cards'],
+      removeAction: divinerActions['Undo Add two rolling [Heal 1 Self] cards'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add two rolling [Curse] cards',
       addAction: divinerActions['Add two rolling [Curse] cards'],
+      removeAction: divinerActions['Undo Add two rolling [Curse] cards'],
     },
     {
       name: 'Primary',
@@ -237,6 +346,10 @@ export class Diviner implements Character {
       addAction:
         divinerActions[
           'Ignore negative scenario effects and add two (+1) cards'
+        ],
+      removeAction:
+        divinerActions[
+          'Undo Ignore negative scenario effects and add two (+1) cards'
         ],
     },
   ];
