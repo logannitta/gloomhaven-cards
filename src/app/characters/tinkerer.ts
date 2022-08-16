12ec59955023d5fcc1ac797f5d25aa255a37890e
Logan Nitta <logan.nitta@gmail.com>
2022-08-15 21:45:01 -0600

    finished removeActions for all classes

--- src/app/characters/tinkerer.ts
+++ src/app/characters/tinkerer.ts
@@ -1,6 +1,6 @@
 import { CardId, Card, basicCards } from '../deck/basic-deck';
 import { Character, Perk } from './character';
-import { BasicPerkAction } from './perk-actions';
+import { BasicPerkAction, removeCard } from './perk-actions';
 
 export const tinkererCards = {
   rollingFire: {
@@ -44,6 +44,16 @@ export const tinkererActions = {
   'Add two rolling [Fire] cards': (deck: Card[]) => {
     return [...deck, tinkererCards.rollingFire, tinkererCards.rollingFire];
   },
+  'Undo Add two rolling [Fire] cards': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.tinkererRollingFire, newDeck);
+      removeCard(CardId.tinkererRollingFire, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
   'Add three rolling [Muddle] cards': (deck: Card[]) => {
     return [
       ...deck,
@@ -52,23 +62,89 @@ export const tinkererActions = {
       tinkererCards.rollingMuddle,
     ];
   },
+  'Undo Add three rolling [Muddle] cards': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.tinkererRollingMuddle, newDeck);
+      removeCard(CardId.tinkererRollingMuddle, newDeck);
+      removeCard(CardId.tinkererRollingMuddle, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
   'Add two (+1) cards': (deck: Card[]) => {
     return [...deck, basicCards.plus1, basicCards.plus1];
+  },
+  'Undo Add two (+1) cards': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.plus1, newDeck);
+      removeCard(CardId.plus1, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
   },
   'Add one (+3) card': (deck: Card[]) => {
     return [...deck, tinkererCards.plus3];
   },
+  'Undo Add one (+3) card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.tinkererPlus3, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
   'Add one (+1) [Wound] card': (deck: Card[]) => {
     return [...deck, tinkererCards.plus1Wound];
+  },
+  'Undo Add one (+1) [Wound] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.tinkererPlus1Wound, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
   },
   'Add one (+1) [Immobilize] card': (deck: Card[]) => {
     return [...deck, tinkererCards.plus1Immobilize];
   },
+  'Undo Add one (+1) [Immobilize] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.tinkererPlus1Immobilize, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
   'Add one (+1) [Heal 2 Self] card': (deck: Card[]) => {
     return [...deck, tinkererCards.plus1Heal2Self];
   },
+  'Undo Add one (+1) [Heal 2 Self] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.tinkererPlus1Heal2Self, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
   'Add one (+0) [Add Target] card': (deck: Card[]) => {
     return [...deck, tinkererCards.plus0AddTarget];
+  },
+  'Undo Add one (+0) [Add Target] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.tinkererPlus0AddTarget, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
   },
 };
 
@@ -81,84 +157,98 @@ export class Tinkerer implements Character {
       completed: false,
       text: 'Remove two (-1) cards',
       addAction: BasicPerkAction['Remove two -1 cards'],
+      removeAction: BasicPerkAction['Undo Remove two -1 cards'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Remove two (-1) cards',
       addAction: BasicPerkAction['Remove two -1 cards'],
+      removeAction: BasicPerkAction['Undo Remove two -1 cards'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Replace one (-2) card with one (+0) card',
       addAction: BasicPerkAction['Replace one -2 with one +0 card'],
+      removeAction: BasicPerkAction['Undo Replace one -2 with one +0 card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add two (+1) cards',
       addAction: tinkererActions['Add two (+1) cards'],
+      removeAction: tinkererActions['Undo Add two (+1) cards'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+3) card',
       addAction: tinkererActions['Add one (+3) card'],
+      removeAction: tinkererActions['Undo Add one (+3) card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add two rolling [Fire] cards',
       addAction: tinkererActions['Add two rolling [Fire] cards'],
+      removeAction: tinkererActions['Undo Add two rolling [Fire] cards'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add three rolling [Muddle] cards',
       addAction: tinkererActions['Add three rolling [Muddle] cards'],
+      removeAction: tinkererActions['Undo Add three rolling [Muddle] cards'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Wound] card',
       addAction: tinkererActions['Add one (+1) [Wound] card'],
+      removeAction: tinkererActions['Undo Add one (+1) [Wound] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Wound] card',
       addAction: tinkererActions['Add one (+1) [Wound] card'],
+      removeAction: tinkererActions['Undo Add one (+1) [Wound] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Immobilize] card',
       addAction: tinkererActions['Add one (+1) [Immobilize] card'],
+      removeAction: tinkererActions['Undo Add one (+1) [Immobilize] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Immobilize] card',
       addAction: tinkererActions['Add one (+1) [Immobilize] card'],
+      removeAction: tinkererActions['Undo Add one (+1) [Immobilize] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Heal 2 Self] card',
       addAction: tinkererActions['Add one (+1) [Heal 2 Self] card'],
+      removeAction: tinkererActions['Undo Add one (+1) [Heal 2 Self] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Heal 2 Self] card',
       addAction: tinkererActions['Add one (+1) [Heal 2 Self] card'],
+      removeAction: tinkererActions['Undo Add one (+1) [Heal 2 Self] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+0) [Add Target] card',
       addAction: tinkererActions['Add one (+0) [Add Target] card'],
+      removeAction: tinkererActions['Undo Add one (+0) [Add Target] card'],
     },
   ];
 }
--- src/app/characters/voidwarden.ts
+++ src/app/characters/voidwarden.ts
@@ -1,4 +1,4 @@
-import { CardId, Card } from '../deck/basic-deck';
+import { CardId, Card, basicCards } from '../deck/basic-deck';
 import { Character, Perk } from './character';
 import { BasicPerkAction, removeCard } from './perk-actions';
 
@@ -44,33 +44,117 @@ export const voidwardenActions = {
   'Add one (+3) card': (deck: Card[]) => {
     return [...deck, voidwardenCards.plus3];
   },
+  'Undo Add one (+3) card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.voidwardenPlus3, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
   'Add one (+1) [Heal 1 Ally] card': (deck: Card[]) => {
     return [...deck, voidwardenCards.plus1Heal1Ally];
+  },
+  'Undo Add one (+1) [Heal 1 Ally] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.voidwardenPlus1Heal1Ally, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
   },
   'Add one (+1) [Curse] card': (deck: Card[]) => {
     return [...deck, voidwardenCards.plus1Curse];
   },
+  'Undo Add one (+1) [Curse] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.voidwardenPlus1Curse, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
   'Add one (+1) [Poison] card': (deck: Card[]) => {
     return [...deck, voidwardenCards.plus1Poison];
   },
+  'Undo Add one (+1) [Poison] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.voidwardenPlus1Poison, newDeck);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
   'Remove one (-2) card': (deck: Card[]) => {
     const newDeck = [...deck];
-    removeCard(CardId.minus2, newDeck);
+    try {
+      removeCard(CardId.minus2, newDeck);
+    } catch (error) {
+      return false;
+    }
     return newDeck;
+  },
+  'Undo Remove one (-2) card': (deck: Card[]) => {
+    return [...deck, basicCards.minus2];
   },
   'Replace one (+0) card with one (+1) [Dark] card': (deck: Card[]) => {
     const newDeck = [...deck];
-    removeCard(CardId.plus0, newDeck, [voidwardenCards.plus1Dark]);
+    try {
+      removeCard(CardId.plus0, newDeck, [voidwardenCards.plus1Dark]);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
+  'Undo Replace one (+0) card with one (+1) [Dark] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.voidwardenPlus1Dark, newDeck, [basicCards.plus0]);
+    } catch (error) {
+      return false;
+    }
     return newDeck;
   },
   'Replace one (+0) card with one (+1) [Ice] card': (deck: Card[]) => {
     const newDeck = [...deck];
-    removeCard(CardId.plus0, newDeck, [voidwardenCards.plus1Ice]);
+    try {
+      removeCard(CardId.plus0, newDeck, [voidwardenCards.plus1Ice]);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
+  'Undo Replace one (+0) card with one (+1) [Ice] card': (deck: Card[]) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.voidwardenPlus1Ice, newDeck, [basicCards.plus0]);
+    } catch (error) {
+      return false;
+    }
     return newDeck;
   },
   'Replace one (-1) card with one (+0) [Heal 1 Ally] card': (deck: Card[]) => {
     const newDeck = [...deck];
-    removeCard(CardId.minus1, newDeck, [voidwardenCards.plus0Heal1Ally]);
+    try {
+      removeCard(CardId.minus1, newDeck, [voidwardenCards.plus0Heal1Ally]);
+    } catch (error) {
+      return false;
+    }
+    return newDeck;
+  },
+  'Undo Replace one (-1) card with one (+0) [Heal 1 Ally] card': (
+    deck: Card[]
+  ) => {
+    const newDeck = [...deck];
+    try {
+      removeCard(CardId.voidwardenPlus0Heal1Ally, newDeck, [basicCards.minus1]);
+    } catch (error) {
+      return false;
+    }
     return newDeck;
   },
 };
@@ -84,12 +168,14 @@ export class Voidwarden implements Character {
       completed: false,
       text: 'Remove two (-1) cards',
       addAction: BasicPerkAction['Remove two -1 cards'],
+      removeAction: BasicPerkAction['Undo Remove two -1 cards'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Remove one (-2) card',
       addAction: voidwardenActions['Remove one (-2) card'],
+      removeAction: voidwardenActions['Undo Remove one (-2) card'],
     },
     {
       name: 'Primary',
@@ -97,35 +183,42 @@ export class Voidwarden implements Character {
       text: 'Replace one (+0) card with one (+1) [Dark] card',
       addAction:
         voidwardenActions['Replace one (+0) card with one (+1) [Dark] card'],
-    },
-    {
-      name: 'Primary',
-      completed: false,
-      text: 'Replace one (+0) card with one (+1) [Dark] card',
-      addAction:
-        voidwardenActions['Replace one (+0) card with one (+1) [Dark] card'],
-    },
-    {
-      name: 'Primary',
-      completed: false,
-      text: 'Replace one (+0) card with one (+1) [Ice] card',
-      addAction:
-        voidwardenActions['Replace one (+0) card with one (+1) [Ice] card'],
-    },
-    {
-      name: 'Primary',
-      completed: false,
-      text: 'Replace one (+0) card with one (+1) [Ice] card',
-      addAction:
-        voidwardenActions['Replace one (+0) card with one (+1) [Ice] card'],
-    },
-    {
-      name: 'Primary',
-      completed: false,
-      text: 'Replace one (-1) card with one (+0) [Heal 1 Ally] card',
-      addAction:
+      removeAction:
         voidwardenActions[
-          'Replace one (-1) card with one (+0) [Heal 1 Ally] card'
+          'Undo Replace one (+0) card with one (+1) [Dark] card'
+        ],
+    },
+    {
+      name: 'Primary',
+      completed: false,
+      text: 'Replace one (+0) card with one (+1) [Dark] card',
+      addAction:
+        voidwardenActions['Replace one (+0) card with one (+1) [Dark] card'],
+      removeAction:
+        voidwardenActions[
+          'Undo Replace one (+0) card with one (+1) [Dark] card'
+        ],
+    },
+    {
+      name: 'Primary',
+      completed: false,
+      text: 'Replace one (+0) card with one (+1) [Ice] card',
+      addAction:
+        voidwardenActions['Replace one (+0) card with one (+1) [Ice] card'],
+      removeAction:
+        voidwardenActions[
+          'Undo Replace one (+0) card with one (+1) [Ice] card'
+        ],
+    },
+    {
+      name: 'Primary',
+      completed: false,
+      text: 'Replace one (+0) card with one (+1) [Ice] card',
+      addAction:
+        voidwardenActions['Replace one (+0) card with one (+1) [Ice] card'],
+      removeAction:
+        voidwardenActions[
+          'Undo Replace one (+0) card with one (+1) [Ice] card'
         ],
     },
     {
@@ -136,48 +229,72 @@ export class Voidwarden implements Character {
         voidwardenActions[
           'Replace one (-1) card with one (+0) [Heal 1 Ally] card'
         ],
+      removeAction:
+        voidwardenActions[
+          'Undo Replace one (-1) card with one (+0) [Heal 1 Ally] card'
+        ],
+    },
+    {
+      name: 'Primary',
+      completed: false,
+      text: 'Replace one (-1) card with one (+0) [Heal 1 Ally] card',
+      addAction:
+        voidwardenActions[
+          'Replace one (-1) card with one (+0) [Heal 1 Ally] card'
+        ],
+      removeAction:
+        voidwardenActions[
+          'Undo Replace one (-1) card with one (+0) [Heal 1 Ally] card'
+        ],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Heal 1 Ally] card',
       addAction: voidwardenActions['Add one (+1) [Heal 1 Ally] card'],
+      removeAction: voidwardenActions['Undo Add one (+1) [Heal 1 Ally] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Heal 1 Ally] card',
       addAction: voidwardenActions['Add one (+1) [Heal 1 Ally] card'],
+      removeAction: voidwardenActions['Undo Add one (+1) [Heal 1 Ally] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Heal 1 Ally] card',
       addAction: voidwardenActions['Add one (+1) [Heal 1 Ally] card'],
+      removeAction: voidwardenActions['Undo Add one (+1) [Heal 1 Ally] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Poison] card',
       addAction: voidwardenActions['Add one (+1) [Poison] card'],
+      removeAction: voidwardenActions['Undo Add one (+1) [Poison] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+3) card',
       addAction: voidwardenActions['Add one (+3) card'],
+      removeAction: voidwardenActions['Undo Add one (+3) card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Curse] card',
       addAction: voidwardenActions['Add one (+1) [Curse] card'],
+      removeAction: voidwardenActions['Undo Add one (+1) [Curse] card'],
     },
     {
       name: 'Primary',
       completed: false,
       text: 'Add one (+1) [Curse] card',
       addAction: voidwardenActions['Add one (+1) [Curse] card'],
+      removeAction: voidwardenActions['Undo Add one (+1) [Curse] card'],
     },
   ];
 }
